a7df1ea5f76e36e000d34c7e30f51ea1ac41e1fa
Zimovets_Serhii <zimovets.s.a@gmail.com>
2023-11-30 11:11:33 +0200

    wifi conect success

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,29 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
+#include <WiFi.h>
 
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
-  Serial.begin(9600);
+const char ssid[] = "TP-Link_9E4C";
+const char password[] = "79053600";
+
+void setup(){
+    Serial.begin(9600);
+    delay(5000);
+
+    WiFi.begin(ssid, password);
+    Serial.println("Connecting");
+
+    wl_status_t status = WL_DISCONNECTED;
+
+    while(status != WL_CONNECTED){
+        status = WiFi.status();
+        Serial.print(status);
+        Serial.print(".");
+        delay(500);
+    }
+
+    Serial.println("Connected to the WiFi network");
+    Serial.print("Local ESP32 IP: ");
+    Serial.println(WiFi.localIP());
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-  Serial.println("Hello World");
-  delay(1000);
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
-}
+void loop(){}
