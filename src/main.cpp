2e5aaab7d5d126e690f98fedd2f147e7ee13217d
Zimovets_Serhii <zimovets.s.a@gmail.com>
2023-12-01 11:00:25 +0200

    wifi test with real api

--- src/main.cpp
+++ src/main.cpp
@@ -1,29 +1,52 @@
 #include <Arduino.h>
 
 #include <WiFi.h>
+#include <HTTPClient.h>
 
 const char ssid[] = "TP-Link_9E4C";
 const char password[] = "79053600";
 
-void setup(){
-    Serial.begin(9600);
-    delay(5000);
+void setup() {
+  Serial.begin(9600);
 
-    WiFi.begin(ssid, password);
-    Serial.println("Connecting");
+  // Connect to Wi-Fi
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.println("Connecting to WiFi...");
+  }
 
-    wl_status_t status = WL_DISCONNECTED;
-
-    while(status != WL_CONNECTED){
-        status = WiFi.status();
-        Serial.print(status);
-        Serial.print(".");
-        delay(500);
-    }
-
-    Serial.println("Connected to the WiFi network");
-    Serial.print("Local ESP32 IP: ");
-    Serial.println(WiFi.localIP());
+  Serial.println("Connected to WiFi");
 }
 
-void loop(){}
+void loop() {
+  if (WiFi.status() == WL_CONNECTED) {
+    HTTPClient http;
+
+    // Your server URL
+    String serverUrl = "https://jsonplaceholder.typicode.com/todos/1";
+
+    // Send the GET request
+    http.begin(serverUrl);
+
+    int httpResponseCode = http.GET();
+
+    if (httpResponseCode > 0) {
+      Serial.print("HTTP Response code: ");
+      Serial.println(httpResponseCode);
+
+      String payload = http.getString();
+      Serial.println("Response payload: ");
+      Serial.println(payload);
+    } else {
+      Serial.print("HTTP Request failed, error: ");
+      Serial.println(httpResponseCode);
+    }
+
+    // Close connection
+    http.end();
+  }
+
+  // Wait for some time before sending the next request
+  delay(5000);
+}
