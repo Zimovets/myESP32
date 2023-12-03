bd1be597bd3d90018b18858a88c25dba4e447390
Zimovets_Serhii <zimovets.s.a@gmail.com>
2023-12-03 12:38:24 +0200

    bluetooth connect

--- src/main.cpp
+++ src/main.cpp
@@ -1,52 +1,25 @@
 #include <Arduino.h>
 
-#include <WiFi.h>
-#include <HTTPClient.h>
+#include "BluetoothSerial.h"
 
-const char ssid[] = "TP-Link_9E4C";
-const char password[] = "79053600";
+#if !defined(CONFIG_BT_ENABLED) || !defined(CONFIG_BLUEDROID_ENABLED)
+#error Bluetooth is not enabled! Please run `make menuconfig` to and enable it
+#endif
+
+BluetoothSerial SerialBT;
 
 void setup() {
   Serial.begin(9600);
-
-  // Connect to Wi-Fi
-  WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(1000);
-    Serial.println("Connecting to WiFi...");
-  }
-
-  Serial.println("Connected to WiFi");
+  SerialBT.begin("ESP32test"); //Bluetooth device name
+  Serial.println("The device started, now you can pair it with bluetooth!");
 }
 
 void loop() {
-  if (WiFi.status() == WL_CONNECTED) {
-    HTTPClient http;
-
-    // Your server URL
-    String serverUrl = "https://jsonplaceholder.typicode.com/todos/1";
-
-    // Send the GET request
-    http.begin(serverUrl);
-
-    int httpResponseCode = http.GET();
-
-    if (httpResponseCode > 0) {
-      Serial.print("HTTP Response code: ");
-      Serial.println(httpResponseCode);
-
-      String payload = http.getString();
-      Serial.println("Response payload: ");
-      Serial.println(payload);
-    } else {
-      Serial.print("HTTP Request failed, error: ");
-      Serial.println(httpResponseCode);
-    }
-
-    // Close connection
-    http.end();
+  if (Serial.available()) {
+    SerialBT.write(Serial.read());
   }
-
-  // Wait for some time before sending the next request
-  delay(5000);
+  if (SerialBT.available()) {
+    Serial.write(SerialBT.read());
+  }
+  delay(20);
 }
